13f201b194763c82192073ed5483598e7ecd5d8c
Tomáš <46355776+Tomecek@users.noreply.github.com>
2025-06-15 22:32:50 +0200

    Update class-train.py

--- class-train.py
+++ class-train.py
@@ -28,41 +28,45 @@ class FeedbackCollector:
 class DataLoader:
     def __init__(self, file_path: Path):
         self.csv_path = file_path
-        self.raw_data = None
-        self.json_columns = []
         
     def load_csv(self, sep=";", quotechar='"', **kwargs) -> pd.DataFrame:
         try:
-            self.raw_data = pd.read_csv(self.csv_path, sep=sep, quotechar=quotechar, **kwargs)
-            print(f"## Loaded number of recors: {len(self.raw_data)}## From file: {self.csv_path}")
-            return self.raw_data
+            df = pd.read_csv(self.csv_path, sep=sep, quotechar=quotechar, **kwargs)
+            print(f"## Loaded number of records: {len(df)}## From file: {self.csv_path}")
+            return df
             
         except Exception as e:
             raise Exception(f"Failed loading file: {e}")
     
-    def normalize_json_cols(self, cols: List[str]) -> pd.DataFrame:
-        if self.raw_data is None:
-            raise ValueError("Value Error")
+
+class DataTransformer:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+    
+    def normalize_json_cols(self, cols: List[str], fields_per_col: Dict[str, List[str]] = None) -> pd.DataFrame:
+        if self.df is None:
+            raise ValueError("Data didn't load")
+        
+        
         for col in cols:
             try:
-                parsed = self.raw_data[col].dropna().apply(json.loads)
+                parsed = self.df[col].dropna().apply(json.loads)
+                
+                if fields_per_col and col in fields_per_col:
+                    # selectively extracting the json fields
+                    parsed  = parsed.apply(lambda d: {k: d.get(k) for k in fields_per_col[col]})
+                    
                 
                 normalized = pd.json_normalize(parsed)
                 normalized.columns = [f"{col}_{c}" for c in normalized.columns]
                 
                 
-                self.raw_data = self.raw_data.drop(columns=[col])
-                self.raw_data = pd.concat([self.raw_data, normalized], axis=1)
+                self.df = self.df.drop(columns=[col])
+                self.df = pd.concat([self.df, normalized], axis=1)
             except Exception as e:
                 print(f"Error with col '{col}': {e}")
             
-        return self.raw_data
-    
-    def data_process(self, json_cols: List[str], **csv_kwargs) -> pd.DataFrame:
-        self.load_csv(**csv_kwargs)
-        self.normalize_json_cols(json_cols)
-        
-        return self.raw_data
+        return self.df
 
 class ColExtractor:
     def __init__(self, df: pd.DataFrame):
@@ -86,13 +90,27 @@ class DataExporter:
         pass
 
 loader = DataLoader(Path(GLOBAL_PATH))
-df_class = loader.data_process(json_cols=["InitialData", "DataAI"])
+df = loader.load_csv()
+transformer = DataTransformer(df)
+#df_class = loader.data_process(json_cols=["InitialData", "DataAI"])
+
+df_transformed = transformer.normalize_json_cols(cols=["InitialData", "DataAI"],
+    fields_per_col={
+        "InitialData": ["Title", "Answer"],
+        "DataAI": ["Answer"]
+    }
+)
+
+print(f"Pokus o write Excelu")
+excel_print = df_transformed.to_excel(excel_writer="kebabmore.xlsx", sheet_name="Jebka Jedna")
+print("Asi jsem printunl?", excel_print)
+
 
 df = pd.read_csv(GLOBAL_PATH, sep=";", quotechar='"')
 print(f"Normální DF columns: {df.columns}, Normální DF shape: {df.shape}")
 
 
-print(f"Tohle jsou columns po loadingu:{df_class.columns}")
+print(f"Tohle jsou columns po loadingu:{df_transformed.columns}")
 print(df["InitialData"])
 print(df["DataAI"])
 
@@ -102,8 +120,8 @@ print(df["DataAI"])
 
 # Extrakce Columns, převod Datumu na správný formát, row_data = {ID row['Id']}
 # Přídání new column podle toho, jestli DataInit (user) == Data (AI) když jo OK, když ne KO, a pokud je aspoň jedno KO, tak addnout nakonec colum s KO
-#  
+# Initial má Title: (Otázka) Answer: (Odpověď) a DataAI(Data) má taky Title: (Otázka) Answer: (Odpověď) if InitialData.Answer == DataAI.Answer OK else KO
+# Pro každý ProcessTypeId novej Sheet (protože každej má jinak velký InitialData a DataAI)
 
-print(f"Pokus o write Excelu")
-excel_print = df_class.to_excel(excel_writer="kebabmore.xlsx", sheet_name="Jebka Jedna")
-print("Asi jsem printunl?", excel_print)
+# Nakonec udělat Jeden co shrne celkovou úspěšnost dokumentů a jednotlivých atributů
+
